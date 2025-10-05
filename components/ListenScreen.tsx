ebaae2bb90a78f69919cf637300f174b5d3ee678
Bryan <morganthemosaic@gmail.com>
2025-10-05 09:36:11 -0400

    Move core of the Listen tabs to component

---
+++ components/ListenScreen.tsx
@@ -0,0 +1,162 @@
+import { FontAwesome } from "@expo/vector-icons";
+import * as Haptics from "expo-haptics";
+import { FlatList, Image, Linking, Pressable, Text, View } from "react-native";
+import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import Animated, {
+    Extrapolation,
+    interpolate,
+    SharedValue,
+    useAnimatedStyle,
+} from "react-native-reanimated";
+import { useList } from "~/context/List";
+import { useSpotify } from "~/context/Spotify";
+import { SpotifyItem } from "~/types";
+
+const ListenScreen = ({ list }: { list: SpotifyItem[] }) => {
+    const { removeFromList } = useList();
+
+    const { user } = useSpotify();
+
+    const renderLeft = (
+        progress: SharedValue<number>,
+        dragX: SharedValue<number>,
+        item: SpotifyItem
+    ) => {
+        const animatedStyle = useAnimatedStyle(() => ({
+            transform: [
+                {
+                    translateX: interpolate(
+                        dragX.value,
+                        [0, 50, 100],
+                        [-100, -50, 0],
+                        Extrapolation.CLAMP
+                    ),
+                },
+            ],
+        }));
+
+        const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+        return (
+            <AnimatedPressable
+                className="flex w-[100px] items-center justify-center rounded-l-lg bg-red-500 active:opacity-80"
+                onPress={async () => {
+                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+
+                    await removeFromList(item, "today");
+                }}
+                style={animatedStyle}>
+                <Text className="text-xl text-zinc-50">Delete</Text>
+            </AnimatedPressable>
+        );
+    };
+
+    return (
+        <>
+            <View className="flex w-full flex-1 flex-row gap-4 px-4">
+                <View className="flex flex-1 flex-row items-center justify-around">
+                    <Pressable
+                        className="active:opacity-80"
+                        disabled={list.length === 0}
+                        onPress={() => {
+                            if (list.length) {
+                                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+                            }
+                        }}>
+                        <View className="flex h-12 w-[72px] items-center justify-center rounded-lg bg-neutral-800">
+                            <Text className="text-xl text-zinc-50">Filter</Text>
+                        </View>
+                    </Pressable>
+                    <Pressable
+                        className="active:opacity-80"
+                        disabled={list.length === 0}
+                        onPress={() => {
+                            if (list.length) {
+                                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+                            }
+                        }}>
+                        <View className="flex h-12 w-[72px] items-center justify-center rounded-lg bg-neutral-800">
+                            <Text className="text-xl text-zinc-50">Sort</Text>
+                        </View>
+                    </Pressable>
+                </View>
+                <Image
+                    className="aspect-square max-h-full max-w-full rounded-lg"
+                    source={{ uri: user?.images?.[0]?.url }}
+                />
+            </View>
+            <View
+                className={`${!list.length && "items-center justify-center"} flex w-full flex-[9] p-4`}>
+                {list.length ? (
+                    <FlatList
+                        contentContainerClassName="gap-4"
+                        data={list}
+                        renderItem={({ item }) => (
+                            <Swipeable
+                                onSwipeableCloseStartDrag={() =>
+                                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+                                }
+                                onSwipeableOpenStartDrag={() =>
+                                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+                                }
+                                overshootLeft={false}
+                                renderLeftActions={(progress, dragX) =>
+                                    renderLeft(progress, dragX, item)
+                                }
+                                renderRightActions={(progress, dragX, swipeable) => null}>
+                                <View className="flex h-[96px] w-full flex-row items-center gap-2 rounded-lg bg-neutral-800 p-2 shadow-lg">
+                                    <Image
+                                        className="aspect-square h-full rounded-lg"
+                                        source={{ uri: item.imageURL }}
+                                    />
+                                    <View className="flex h-full flex-1 justify-around">
+                                        <Text className="text-xl text-zinc-50" numberOfLines={1}>
+                                            {item.name}
+                                        </Text>
+                                        <Text className="text-zinc-50/80" numberOfLines={1}>
+                                            {item.type[0].toUpperCase()}
+                                            {item.type.slice(1, item.type.length - 1)}
+                                        </Text>
+                                    </View>
+                                    <Pressable
+                                        className="active:opacity-80"
+                                        onPress={async () => {
+                                            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+
+                                            try {
+                                                const canOpen = await Linking.canOpenURL(item.uri);
+
+                                                if (canOpen) {
+                                                    await Linking.openURL(item.uri);
+                                                } else {
+                                                    await Linking.openURL(
+                                                        item.uri
+                                                            .split(":")
+                                                            .join("/")
+                                                            .replace(
+                                                                "spotify",
+                                                                "https://open.spotify.com"
+                                                            )
+                                                    );
+                                                }
+                                            } catch (error) {
+                                                console.error("Error opening Spotify:", error);
+                                            }
+                                        }}>
+                                        <View className="flex h-12 w-12 items-center justify-center rounded-full bg-green-500">
+                                            <FontAwesome name="play" size={20} />
+                                        </View>
+                                    </Pressable>
+                                </View>
+                            </Swipeable>
+                        )}
+                    />
+                ) : (
+                    <Text className="text-xl text-zinc-50">Add Items from the Search tab</Text>
+                )}
+            </View>
+        </>
+    );
+};
+
+export default ListenScreen;
