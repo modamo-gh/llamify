cb218be5833208e831880e28bd1702e8f78690ec
Bryan <morganthemosaic@gmail.com>
2025-10-13 10:42:08 -0400

    Add donation link

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
-import { Image, Pressable, Text, View } from "react-native";
+import { Image, Linking, Pressable, Text, View } from "react-native";
 import { useSpotify } from "~/context/Spotify";
 
 const Login = () => {
@@ -19,13 +19,18 @@ const Login = () => {
             <View className="flex flex-[4] items-center justify-center">
                 <Image className="h-96 w-96" source={require("../assets/playstore.png")} />
             </View>
-            <View className="flex-1">
+            <View className="flex flex-1 justify-around">
                 <Pressable
                     className="flex h-12 w-64 items-center justify-center rounded-lg bg-green-500 px-2 py-1 active:bg-green-600"
                     onPress={() => {
                         promptAsync();
                     }}>
                     <Text className="text-lg font-semibold text-zinc-50">Login with Spotify</Text>
+                </Pressable>
+                <Pressable
+                    className="flex h-12 w-64 items-center justify-center"
+                    onPress={() => Linking.openURL("https://buymeacoffee.com/modamo")}>
+                    <Text className="text-green-500 underline">☕️ Buy me a coffee</Text>
                 </Pressable>
             </View>
         </View>
