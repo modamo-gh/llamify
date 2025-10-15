4b2e875d87ad4dbbdfc19a64c54c65ba4b294617
Bryan <morganthemosaic@gmail.com>
2025-10-14 22:07:27 -0400

    Add the layout back

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,22 @@
-// import "react-native-gesture-handler";
-
-// import { Stack } from "expo-router";
-// import { GestureHandlerRootView } from "react-native-gesture-handler";
-// import { ListProvider } from "~/legacy_removed/context/List";
-// import { SpotifyProvider } from "~/legacy_removed/context/Spotify";
-// import "../global.css";
-
-// export const unstable_settings = {
-//     initialRouteName: "login",
-// };
-
-// export default function RootLayout() {
-//     return (
-//         <SpotifyProvider>
-//             <ListProvider>
-//                 <GestureHandlerRootView style={{ flex: 1 }}>
-//                     <Stack>
-//                         <Stack.Screen name="index" options={{ headerShown: false }} />
-//                         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-//                     </Stack>
-//                 </GestureHandlerRootView>
-//             </ListProvider>
-//         </SpotifyProvider>
-//     );
-// }
-
-// app/_layout.tsx
 import { Stack } from "expo-router";
+import { ListProvider } from "~/context/List";
+import { SpotifyProvider } from "~/context/Spotify";
+import "../global.css";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen
-                name="index"
-                options={{
-                    title: "Home",
-                }}
-            />
-        </Stack>
+        <SpotifyProvider>
+            <ListProvider>
+                <Stack>
+                    <Stack.Screen
+                        name="index"
+                        options={{
+                            headerShown: false,
+                            title: "Login",
+                        }}
+                    />
+                </Stack>
+            </ListProvider>
+        </SpotifyProvider>
     );
 }
